865df4ec7bbd4b31fcdedb2f57f1025a62ebc5a1
Mislav Marohnić <mislav.marohnic@gmail.com>
2011-02-18 17:56:26 +0100

    ruby 1.9.2: use syck instead of Psych

--- lib/choices.rb
+++ lib/choices.rb
@@ -16,7 +16,7 @@ module Choices
   
   def load_settings_hash(filename, env)
     yaml_content = ERB.new(IO.read(filename)).result
-    YAML::load(yaml_content)[env]
+    yaml_load(yaml_content)[env]
   end
   
   def with_local_settings(filename, env, suffix)
@@ -24,6 +24,19 @@ module Choices
     if File.exists? local_filename
       hash = load_settings_hash(local_filename, env)
       yield hash if hash
+    end
+  end
+  
+  def yaml_load(content)
+    if defined? YAML::ENGINE
+      # avoid using broken Psych in 1.9.2
+      old_yamler = YAML::ENGINE.yamler
+      YAML::ENGINE.yamler = 'syck'
+    end
+    begin
+      YAML::load(content)
+    ensure
+      YAML::ENGINE.yamler = old_yamler if defined? YAML::ENGINE
     end
   end
 end
