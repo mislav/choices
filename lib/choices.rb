69be8a3b78bbf2342d8fc37367d019e652f6473b
Pier-Olivier Thibault <pothibo@gmail.com>
2013-07-17 18:59:16 -0400

    raise error if key for the current environment is missing

--- lib/choices.rb
+++ lib/choices.rb
@@ -6,24 +6,27 @@ module Choices
   extend self
   
   def load_settings(filename, env)
-    mash = Hashie::Mash.new(load_settings_hash(filename, env))
+    mash = Hashie::Mash.new(load_settings_hash(filename))
     
-    with_local_settings(filename, env, '.local') do |local|
+    with_local_settings(filename, '.local') do |local|
       mash.update local
     end
     
-    return mash
+    unless mash.has_key? env
+      raise EnvironmentMissingError, "You are missing environment (#{env}) in #{filename}."
+    end
+    return mash[env]
   end
-  
-  def load_settings_hash(filename, env)
+
+  def load_settings_hash(filename)
     yaml_content = ERB.new(IO.read(filename)).result
-    yaml_load(yaml_content)[env]
+    yaml_load(yaml_content)
   end
   
-  def with_local_settings(filename, env, suffix)
+  def with_local_settings(filename, suffix)
     local_filename = filename.sub(/(\.\w+)?$/, "#{suffix}\\1")
     if File.exists? local_filename
-      hash = load_settings_hash(local_filename, env)
+      hash = load_settings_hash(local_filename)
       yield hash if hash
     end
   end
@@ -40,6 +43,8 @@ module Choices
       YAML::ENGINE.yamler = old_yamler if defined?(YAML::ENGINE) && defined?(Syck)
     end
   end
+
+  class EnvironmentMissingError < StandardError; end;
 end
 
 if defined? Rails
